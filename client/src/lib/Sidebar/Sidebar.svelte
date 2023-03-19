b8dbe05e5f07dbc31b7fd440c158ab34764119b5
jjones <jimmy.jones@tutanota.com>
2023-03-18 21:16:41 -0600

    fix sidebar height

--- client/src/lib/Sidebar/Sidebar.svelte
+++ client/src/lib/Sidebar/Sidebar.svelte
@@ -1,1 +1,3 @@
-<aside class="hidden lg:flex flex-col p-3 w-60 bg-white shadow-lg shadow-slate-800">hello</aside>
+<aside class="hidden lg:flex flex-col h-screen p-3 w-60 bg-white shadow-lg shadow-slate-800">
+	hello
+</aside>
